33e3ba606eefbdab3d2479e9753643270c546d37
Brady Edwards <bwe731@gmail.com>
2024-12-29 12:22:45 -0600

    created initial python code and added shell script

---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+    normal = "normal"
+    bold = "bold"
+    italic = "italic"
+    code = "code"
+    links = "links"
+    images = "images"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(object, TextNode):
+            return False
+        else:
+            return vars(self) == vars(other)
