12a21b5b32f9d1567e388c1f45adc90102a07ed4
Brady Edwards <bwe731@gmail.com>
2024-12-31 11:43:54 -0600

    cleaned up textnode and added unit testing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import *
 
 def main():
-    new_obj = TextNode("hello", "normal")
-
+    new_obj = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(new_obj.__repr__())
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    normal = "normal"
-    bold = "bold"
-    italic = "italic"
-    code = "code"
-    links = "links"
-    images = "images"
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -15,10 +15,12 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        if not isinstance(object, TextNode):
-            return False
-        else:
-            return vars(self) == vars(other)
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
         
     def __repr__(self):
-        type(self)
+        # TextNode(TEXT, TEXT_TYPE, URL)
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
