d0994f283ae1225ab10dbaa3d6f9b64aaff00073
Brady Edwards <bwe731@gmail.com>
2025-01-02 23:36:57 -0600

    removed function from html node classes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -62,21 +62,3 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
-        
-
-def text_node_to_html_node(text_node):
-        if isinstance(text_node.text_type, TextType) == False:
-            raise Exception("Invalid text type")
-        match text_node.text_type.value:
-            case "normal":
-                return LeafNode(None, text_node.text)
-            case "bold":
-                return LeafNode("b", text_node.text)
-            case "italic":
-                return LeafNode("i", text_node.text)
-            case "code":
-                return LeafNode("code", text_node.text)
-            case "link":
-                return LeafNode("a", text_node.text, {"href": text_node.url})
-            case "image":
-                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
