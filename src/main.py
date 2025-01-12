1234c7865064d86a8ac390930f12ee1f5f96d7b9
Brady Edwards <bwe731@gmail.com>
2025-01-11 23:28:24 -0600

    started work on functions in main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import os
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, ParentNode, LeafNode
 
 def main():
     new_obj = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(new_obj.__repr__())
+
+def copy_static_to_public():
+    if not os.path.exists("./static"):
+        raise Exception("Static filepath does not exist")
+    
+
 
 main()
